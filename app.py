f95ec5995eeaf851215ad4579ed0287ee4342467
urgoodfriend-py <ktkim2116@gmail.com>
2020-10-29 16:33:21 +0900

    five

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import nvapi
+
 app = Flask(__name__)
 
 @app.route('/') # '/' 메인화면, 127.0.0.1
@@ -9,12 +11,19 @@ def hello():
 def kb():
     return "kbbaseball!"
 
-@app.route('/face') # '/' 서브화면, 127.0.0.1/face
+@app.route('/face', methods=['GET', 'POST']) # '/' 서브화면, 127.0.0.1/face
 def face():
-    return render_template('face.html')
+    if request.method == 'GET':
+        return render_template('face.html')
+    else:
+        imgurl = request.form['imgurl']
+        # 여기에 face()
+        name = nvapi.justify(imgurl)
+        print(name)
+        return name
 
 if __name__ == '__main__':
-    app.run(port=80) # flask default port = 5000, web default 80번 변경
+    app.run(port=80, debug=True) # flask default port = 5000, web default 80번 변경
 #    app.run(host='0.0.0.0', port=80) # 루프백이 아닌 외부와 주소 연결 설정, 다 열어준다.
 
 # git remote add origin https://github.com/urgoodfriend-py/web.git
---
+++ nvapi.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import requests
+from urllib.request import urlopen
+import json 
+
+def justify(imgurl):
+    client_id = "tz5oef8zdq"
+    client_secret = "JVwWZdVw5NUwKQGSZnRzdiNzvAdAK0Ci21roDIVA"
+    url = "https://naveropenapi.apigw.ntruss.com/vision/v1/celebrity" # 유명인 얼굴인식
+
+    img = urlopen(imgurl).read()
+    files = {'image': img}
+    # files = {'image': open('aaa.jpeg', 'rb')}
+    headers = {'X-NCP-APIGW-API-KEY-ID': client_id, 'X-NCP-APIGW-API-KEY': client_secret }
+    response = requests.post(url,  files=files, headers=headers)
+    rescode = response.status_code
+    if(rescode==200): # ok라는 응답
+        print(response.text)
+        json_dic = json.loads(response.text)
+        # print(json_dic.get('faces')[0].get('celebrity').get('value'))
+        return json_dic.get('faces')[0].get('celebrity').get('value')
+    else:
+        print("Error Code:" + rescode)
