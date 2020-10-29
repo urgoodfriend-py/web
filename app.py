18a4c947c1e12c82b75809df7666f67be8348a5b
urgoodfriend-py <ktkim2116@gmail.com>
2020-10-29 14:59:36 +0900

    three

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/') # '/' 메인화면, 127.0.0.1
@@ -9,8 +9,13 @@ def hello():
 def kb():
     return "kbbaseball!"
 
+@app.route('/face') # '/' 서브화면, 127.0.0.1/face
+def face():
+    return render_template('face.html')
+
 if __name__ == '__main__':
     app.run(port=80) # flask default port = 5000, web default 80번 변경
+#    app.run(host='0.0.0.0', port=80) # 루프백이 아닌 외부와 주소 연결 설정, 다 열어준다.
 
 # git remote add origin https://github.com/urgoodfriend-py/web.git
 # git branch -M main
