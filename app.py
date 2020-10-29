42f3d30f9a1977c49660e2be0a442101710ac5c5
urgoodfriend-py <ktkim2116@gmail.com>
2020-10-29 11:43:01 +0900

    two

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') # '/' 메인화면, 127.0.0.1
 def hello():
     return "Hello World!"
 
+@app.route('/kb') # '/' 서브화면, 127.0.0.1/kb
+def kb():
+    return "kbbaseball!"
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=80) # flask default port = 5000, web default 80번 변경
 
 # git remote add origin https://github.com/urgoodfriend-py/web.git
 # git branch -M main
